6ab56282588dbcc5e0ca4696eaee5c4fa7df511e
JOSSEP-ENG <58581167+JOSSEP-ENG@users.noreply.github.com>
2025-04-22 17:41:08 +0900

    Time: 13 ms (94.88%), Space: 42.1 MB (94.68%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,39 +1,72 @@
+// 투포인터, 슬라이드 윈도우 방식으로 해결
+// 직접 해결한 방식으로는 성능 문제 발생 (완전 탐색 방식 : (On3제곱))
+// 풀이 참고하여 문제 해결.
 class Solution {
+    int left, maxLen;
+
     public String longestPalindrome(String s) {
-        int n = s.length();
+        int len = s.length();
 
-        // 예외 처리: 문자열이 비거나 1글자일 경우, 그 자체가 팰린드롬
-        if (n < 2) return s;
-
-        boolean[][] dp = new boolean[n][n]; // dp[i][j]는 s[i..j]가 팰린드롬이면 true
-        int start = 0;  // 가장 긴 팰린드롬의 시작 인덱스
-        int maxLen = 1; // 가장 긴 팰린드롬의 길이
-
-        // 길이 1: 모든 단일 문자는 팰린드롬
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = true;
+        if (len < 2) {
+            return s;
         }
 
-        // 길이 2 이상인 부분 문자열을 순차적으로 검사
-        for (int len = 2; len <= n; len++) {
-            for (int i = 0; i <= n - len; i++) {
-                int j = i + len - 1; // 끝 인덱스
-
-                if (s.charAt(i) == s.charAt(j)) {
-                    if (len == 2 || dp[i + 1][j - 1]) {
-                        dp[i][j] = true;
-
-                        if (len > maxLen) {
-                            maxLen = len;
-                            start = i;
-                        }
-                    }
-                }
-                // else 생략: 어차피 기본값은 false
-            }
+        for (int i = 0; i < len - 1; i++) {
+            extendPalindrome(s, i, i + 1);
+            extendPalindrome(s, i, i + 2);
         }
 
-        // 가장 긴 팰린드롬 부분 문자열 반환
-        return s.substring(start, start + maxLen);
+        return s.substring(left, left + maxLen);
+    }
+
+    private void extendPalindrome(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+
+        if (maxLen < k - j - 1) {
+            left = j + 1;
+            maxLen = k - j - 1;
+        }
     }
 }
+
+// // 가장 긴 팰린드룸 (LeetCode 5번 문제)
+// // 내가 풀었던 방식은 완전 탐색 방식(Brute Force)
+// // 시간 복잡도 O(n3)
+// // 동작은 가능하지만 문자열이 길수록 성능에 매우 안좋음
+// class Solution {
+//     public String longestPalindrome(String s) {
+//         char[] chars = s.toCharArray();
+//         Set<String> palindromeSet = new HashSet<>();
+
+//         if (s.length() < 2) {
+//             return s;
+//         }
+
+//         for (int i = 0; i < chars.length; i++) {
+//             for (int j = i + 1; j <= chars.length; j++) {
+//                 String substr = s.substring(i, j);
+//                 if (isPalindrome(substr)) {
+//                     palindromeSet.add(substr);
+//                 }
+//             }
+//         }
+
+//         return palindromeSet.stream()
+//                 .max(Comparator.comparingInt(String::length))
+//                 .orElse("");
+//     }
+
+//     private boolean isPalindrome(String s) {
+//         int left = 0;
+//         int right = s.length() - 1;
+//         while (left < right) {
+//             if (s.charAt(left++) != s.charAt(right--)) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
