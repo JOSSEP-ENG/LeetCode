7ba929b85810b647a408fecc003b6306c40619f2
JOSSEP-ENG <58581167+JOSSEP-ENG@users.noreply.github.com>
2025-04-23 21:18:51 +0900

    Time: 2 ms (98.82%), Space: 45.1 MB (46.82%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,23 +1,37 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        List<Integer> results = new ArrayList<>();
-				// 2. target에서 뺀 수가 입력값에 있는지 조회 (Running Time : 38ms) 
+        Map<Integer, Integer> numsMap = new HashMap<>();
+        // 3. HashMap 활용
         for (int i = 0; i < nums.length; i++) {
-            int value = target - nums[i];
-            for (int j = i + 1; j < nums.length; j++) {
-                if (value == nums[j]) {
-                    results.add(i);
-                    results.add(j);
-                    break;
-                }
+            if (numsMap.containsKey(target - nums[i])) {
+                return new int[]{numsMap.get(target - nums[i]), i};
             }
+            numsMap.put(nums[i], i);
         }
-
-        return results.stream()
-                .mapToInt(Integer::intValue)
-                .toArray();
+        return new int[0];
     }
 }
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         List<Integer> results = new ArrayList<>();
+// 				// 2. target에서 뺀 수가 입력값에 있는지 조회 (Running Time : 38ms) 
+//         for (int i = 0; i < nums.length; i++) {
+//             int value = target - nums[i];
+//             for (int j = i + 1; j < nums.length; j++) {
+//                 if (value == nums[j]) {
+//                     results.add(i);
+//                     results.add(j);
+//                     break;
+//                 }
+//             }
+//         }
+
+//         return results.stream()
+//                 .mapToInt(Integer::intValue)
+//                 .toArray();
+//     }
+// }
 
 // class Solution {
 //     public int[] twoSum(int[] nums, int target) {
