d2d0de1df9338fb4e6b538ab01f276e243fed5ea
JOSSEP-ENG <58581167+JOSSEP-ENG@users.noreply.github.com>
2025-04-23 21:02:44 +0900

    Time: 35 ms (44.06%), Space: 44.8 MB (88.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,23 +1,39 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // nums = 2,7,11,15
-        // target = 9
-        // 2+7 = 9
-        // results = {2,7}
         List<Integer> results = new ArrayList<>();
-
-        // 1. 완전 탐색 방법
+				// 2. target에서 뺀 수가 입력값에 있는지 조회 (Running Time : 38ms) 
         for (int i = 0; i < nums.length; i++) {
+            int value = target - nums[i];
             for (int j = i + 1; j < nums.length; j++) {
-                if (target == nums[i] + nums[j]) {
+                if (value == nums[j]) {
                     results.add(i);
                     results.add(j);
+                    break;
                 }
             }
-
         }
+
         return results.stream()
                 .mapToInt(Integer::intValue)
                 .toArray();
     }
 }
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         List<Integer> results = new ArrayList<>();
+
+//         // 1. 완전 탐색 방법 (Running Time : 64ms)
+//         for (int i = 0; i < nums.length; i++) {
+//             for (int j = i + 1; j < nums.length; j++) {
+//                 if (target == nums[i] + nums[j]) {
+//                     results.add(i);
+//                     results.add(j);
+//                 }
+//             }
+//         }
+//         return results.stream()
+//                 .mapToInt(Integer::intValue)
+//                 .toArray();
+//     }
+// }
